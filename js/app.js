02b625c7ef320caddd7b9124685c8fdada7d42ec
Malak Momani <malak.momani.96@gmail.com>
2021-02-10 15:31:53 +0200

    Malak Momani modifying, malak is driver

--- js/app.js
+++ js/app.js
@@ -1,95 +1,117 @@
-'use strict'
-let mark=0;
+'use strict';
+
+
+let mark = 0;
 let userName = prompt("Whats Your Name");
-document.write("Hello" + "  " + userName + " " );
-let userHobby= prompt("Software development is one of my hobbies? y/n");
+document.write("Hello" + "  " + userName + " ");
 
-if(userHobby.toLocaleUpperCase() === "Y" || userHobby.toUpperCase()==="YES"){
-    alert("Ohhhh Its Correct");
-    mark++
-}else{
-    alert("Obbs its my favorite hobbie");
-}
-let userMovies=prompt("This type of movies I like it (Science,Science Fiction and Action)? y/n")
-if(userMovies.toLowerCase() === "y" || userMovies.toLowerCase() === "yes"){
-    alert("Ohhhh Its Correct");
-    mark++;
-}else{
-    alert("Obbs I like This type of movies");
-}
-let userUniversity=prompt("Did I graduate From Jordan university of science and technology? y/n");
-if(userUniversity.toLowerCase() === "y" || userUniversity.toLowerCase() === "yes"){
-    alert("Ohhhh Its Correct")
-    mark++;
-}else{
-    alert("Obbs I graduate From JUST");
-}
-let userDish = prompt("Is Mansaf my favorite dish? y/n");
-if(userDish.toLowerCase()=== "y" || userDish.toLowerCase() === "yes"){
-    alert("Ohhhh Its Correct");
-    mark++;
-}else{
-    alert('Obbs you must meet me in Grand Omaya Restaurant and Cafe- Irbid');
-}
-let userClub = prompt("Is Real Madrid my favorite football club ? y/n");
-if(userDish.toLowerCase()=== "y" || userDish.toLowerCase() === "yes"){
-    alert("Ohhhh Its Correct");
-    mark++;
-}else{
-    alert("Obbs Real Madrid my favorite football club");
+
+function userAnswer(question) {
+    let userInput = prompt(question);
+    return userInput;
 }
 
-let correctAnswer = 3;
-let userGuess;
-for(let i=3 ; i>=0 ; i--){
-userGuess = prompt("How long do I have in customer service experience??")
-    
-    if (parseInt(userGuess) === correctAnswer) {
-        alert("Ohhhh Thats Correct")
-        mark++;
-        break;
-    }else if(parseInt(userGuess)>correctAnswer && i>0){
-        alert("Obbs Your answer is too high you have only " + i + " opportunities")
+let myQuestions = ["Software development is one of my hobbies? y/n",
+    "This type of movies I like it (Science,Science Fiction and Action)? y/n",
+    "Did I graduate From Jordan university of science and technology? y/n",
+    "Is Mansaf my favorite dish? y/n",
+    "Is Real Madrid my favorite football club ? y/n",
+    "How long do I have in customer service experience??",
+    "Wahts your guess about cities i fisted in jordan?"]
 
-    }else if(parseInt(userGuess)<correctAnswer && i>0){
-        alert("Obbs Your answer is too low you have only " + i + " opportunities")
-    }else if(i>0){
-        //alert(" you have only " + i + " opportunities");
-    }
+//console.log(myQuestion.length);
+let userAnswers = [" ", " ", " ", " ", " ", " ", " "];
 
-        else if(i === 0)
-        {
-        alert("I have 3 Years experience in customer service");
-        }
-    }
-
-    let myCity = ["amman","alzarqa","almafraq","alkarak"];
-    let userCity;
-
-    for(let j=5 ; j >0 ; j--){
-
-        
-        // for (let n=0 ; n<=5 ; n++){
-            userCity = prompt("Wahts your guess about cities i fisted in jordan?")
-        // if(userCity.toLocaleLowerCase === myCity )
-        if(userCity.toLowerCase() === myCity[0] || userCity.toLowerCase() === myCity[1] || userCity.toLowerCase() === myCity[2] || userCity.toLowerCase() === myCity[3])
-        {
-            alert("Ohhh its correct");
-            mark++;
+for (let i = 0; i < 7; i++) {
+    userAnswers[i] = userAnswer(myQuestions[i]);
+    switch (i) {
+        case 0:
+            if (userAnswers[0].toLocaleUpperCase() === "Y" || userAnswers[0].toUpperCase() === "YES") {
+                alert("Ohhhh Its Correct");
+                mark++;
+            } else {
+                alert("Obbs its my favorite hobbie");
+            }
             break;
-        }else {
-            alert("Ohhh Its wrong try again you have only " + j + " opportunities");
-        }
-        // }
+        case 1:
+            if (userAnswers[1].toLowerCase() === "y" || userAnswers[1].toLowerCase() === "yes") {
+                alert("Ohhhh Its Correct");
+                mark++;
+            } else {
+                alert("Obbs I like This type of movies");
+            }
+            break;
+        case 2:
+            if (userAnswers[2].toLowerCase() === "y" ||userAnswers[2].toLowerCase() === "yes") {
+                alert("Ohhhh Its Correct")
+                mark++;
+            } else {
+                alert("Obbs I graduate From JUST");
+            }
+            break;
+        case 3:
+            if (userAnswers[3].toLowerCase() === "y" || userAnswers[3].toLowerCase() === "yes") {
+                alert("Ohhhh Its Correct");
+                mark++;
+            } else {
+                alert('Obbs you must meet me in Grand Omaya Restaurant and Cafe- Irbid');
+            }
+            break;
+        case 4:
+            if (userAnswers[4].toLowerCase() === "y" || userAnswers[4].toLowerCase() === "yes") {
+                alert("Ohhhh Its Correct");
+                mark++;
+            } else {
+                alert("Obbs Real Madrid my favorite football club");
+            }
+            break;
+        case 5:
+            let correctAnswer = 3;
+            for (let i = 2; i >= 0; i--) {
+                userAnswers[5]= prompt(myQuestions[5]);
+
+                if (parseInt(userAnswers[5]) === correctAnswer) {
+                    alert("Ohhhh Thats Correct")
+                    mark++;
+                    break;
+                } else if (parseInt(userAnswers[5]) > correctAnswer && i > 0) {
+                    alert("Obbs Your answer is too high you have only " + i + " opportunities")
+
+                } else if (parseInt(userAnswers[5]) < correctAnswer && i > 0) {
+                    alert("Obbs Your answer is too low you have only " + i + " opportunities")
+                } else if (i > 0) {
+                    //alert(" you have only " + i + " opportunities");
+                }
+                else if (i === 0) {
+                    alert("I have 3 Years experience in customer service");
+                }
+            }
+            break;
+        case 6:
+            let myCity = ["amman", "alzarqa", "almafraq", "alkarak"];
+            let x = 0;
+
+            for (let j = 5; j > 0; j--) {
+
+
+                // for (let n=0 ; n<=5 ; n++){
+                userAnswers[6] = prompt("Wahts your guess about cities i fisted in jordan?")
+                // if(userCity.toLocaleLowerCase === myCity )
+                if (userAnswers[6].toLowerCase() === myCity[0] || userAnswers[6].toLowerCase() === myCity[1] || userAnswers[6].toLowerCase() === myCity[2] || userAnswers[6].toLowerCase() === myCity[3]) {
+                    alert("Ohhh its correct");
+                    mark++;
+                    x = 1 ;
+                    break;
+                } else {
+                    alert("Ohhh Its wrong try again you have only " + j + " opportunities");
+                }
+                // }
+            }
+            if(x = 0){
+            alert(`correct answer is ${myCity[0]} , ${myCity[1]}, ${myCity[2]} and ${myCity[3]}`);
+            }
+            break;
+        default:
+            break;
     }
-
-    document.write("Your correct answer score" + " " + mark + " " +"out of 7");
-
-
-
-// console.log('userDish' + userDish);
-// console.log('userHobby' + userHobby);
-// console.log('userName' + userName);
-// console.log('userUniversity' + userUniversity );
-// console.log('userMovies' + userMovies);
-// console.log('userClub' + userClub);
+}
